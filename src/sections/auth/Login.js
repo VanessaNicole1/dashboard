3bbf7cd84a5347d3febc706991c1e71365347616
VanessaNicole1 <nicolegualan@gmail.com>
2022-12-04 11:24:22 -0500

    Add Login Section.

---
+++ src/sections/auth/Login.js
@@ -0,0 +1,31 @@
+import { Alert, Tooltip, Stack, Typography, Box } from '@mui/material';
+import { useAuthContext } from '../../auth/useAuthContext';
+import LoginLayout from '../../layouts/login';
+import AuthLoginForm from './AuthLoginForm';
+
+export default function Login() {
+  const { method } = useAuthContext();
+
+  return (
+    <LoginLayout title='Plan de Clases'>
+      <Stack spacing={2} sx={{ mb: 5, position: 'relative' }}>
+        <Typography variant="h4">Iniciar Sesión</Typography>
+
+        <Tooltip title={method} placement="left">
+          <Box
+            component="img"
+            alt={method}
+            src={`/assets/icons/auth/ic_${method}.png`}
+            sx={{ width: 32, height: 32, position: 'absolute', right: 0 }}
+          />
+        </Tooltip>
+      </Stack>
+
+      <Alert severity="info" sx={{ mb: 3 }}>
+        Use email : <strong>demo@minimals.cc</strong> / password :<strong> demo1234</strong>
+      </Alert>
+
+      <AuthLoginForm />
+    </LoginLayout>
+  );
+}
