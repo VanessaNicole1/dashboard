9c9203e4250725499bb1fd5ffda100185ef1c690
Vanessa <vanessainiguez98@gmail.com>
2022-12-08 18:13:20 -0500

    Create a new path to student, teachers, grades and subjects

--- src/routes/index.js
+++ src/routes/index.js
@@ -13,11 +13,25 @@ import LessonPlanListStudentPage from "../pages/dashboard/lesson-plan/LessonPlan
 import LessonPlanListStudentPendingsPage from "../pages/dashboard/lesson-plan/LessonPlanListStudentsPendingsPage";
 import LessonPlanListTeacherPage from "../pages/dashboard/lesson-plan/LessonPlanListTeacherPage";
 import LessonPlanProcessPage from "../pages/dashboard/lesson-plan/LessonPlanProcessPage";
+import RolesCreatePage from "../pages/dashboard/roles/RolesCreatePage";
+import RolesListPage from "../pages/dashboard/roles/RolesListPage";
+import StudentsCreatePage from "../pages/dashboard/students/StudentsCreatePage";
+import StudentsListPage from "../pages/dashboard/students/StudentsListPage";
+import TeachersCreatePage from "../pages/dashboard/teachers/TeachersCreatePage";
+import TeachersListPage from "../pages/dashboard/teachers/TeachersListPage";
+import GradesCreatePage from "../pages/dashboard/grades/GradesCreatePage";
+
 
 
 import {
   LoginPage
 } from './elements';
+import GradesListPage from "../pages/dashboard/grades/GradesListPage";
+import DegreeViewPage from "../pages/dashboard/degree/DegreeViewPage";
+import SubjectsListPage from "../pages/dashboard/subjects/SubjectsListPage";
+import SubjectsCreatePage from "../pages/dashboard/subjects/SubjectsCreatePage";
+import ScheduleViewPage from "../pages/dashboard/schedule/ScheduleViewPage";
+import ScheduleCreatePage from "../pages/dashboard/schedule/ScheduleCreatePage";
 
 export default function Router () {
 
@@ -46,7 +60,7 @@ export default function Router () {
       { 
         path: 'lesson-plan',
         children: [
-          { element: <Navigate to='/dashboard/lesson-plan/about' replace/>, index: true },
+          { element: <Navigate to='/dashboard/lesson-plan/about' replace />, index: true },
           { path: 'about', element: <LessonPlanAboutPage />},
           { 
             path: 'start-process', 
@@ -56,17 +70,151 @@ export default function Router () {
               </RoleBasedGuard>  
             )
           },
-          { path: 'create', element: <LessonPlanCreatePage />},
+          { path: 'create', 
+            element: (
+              <RoleBasedGuard hasContent roles={[ROLES.manager]}>
+                <LessonPlanCreatePage />
+              </RoleBasedGuard>
+            )},
           { path: 'all', element: <LessonPlanListPage />},
           { path: 'teacher/list', element: <LessonPlanListTeacherPage />},
           { path: 'student/list', element: <LessonPlanListStudentPage />},
           { path: 'student/pendings', element: <LessonPlanListStudentPendingsPage />},
         ]
-      }
+      },
+      { 
+        path: 'roles',
+        children: [
+          { element: <Navigate to='/dashboard/roles/list' replace />, index: true },
+          { path: 'list', element: (
+            <RoleBasedGuard hasContent roles={[ROLES.manager]}>
+               <RolesListPage />
+            </RoleBasedGuard>
+          )},
+          {
+            path: 'create', 
+            element: (
+              <RoleBasedGuard hasContent roles={[ROLES.manager]}>
+                <RolesCreatePage />
+              </RoleBasedGuard>
+            )},
+        ]
+      },
+      { 
+        path: 'teachers',
+        children: [
+          { element: <Navigate to='/dashboard/teachers/list' replace />, index: true },
+          { path: 'list', element: (
+            <RoleBasedGuard hasContent roles={[ROLES.manager]}>
+               <TeachersListPage />
+            </RoleBasedGuard>
+          )},
+          {
+            path: 'create',
+            element: (
+              <RoleBasedGuard hasContent roles={[ROLES.manager]}>
+                <TeachersCreatePage />
+              </RoleBasedGuard>
+            )
+          },
+        ]
+      },
+      {
+        path: 'students',
+        children: [
+          { element: <Navigate to='/dashboard/students/list' replace />, index: true },
+          { path: 'list', 
+            element: (
+              <RoleBasedGuard hasContent roles={[ROLES.manager]}>
+                <StudentsListPage />
+              </RoleBasedGuard>
+            )
+          },
+          {
+            path: 'create',
+            element: (
+              <RoleBasedGuard hasContent roles={[ROLES.manager]}>
+                <StudentsCreatePage />
+              </RoleBasedGuard>
+            )
+          },
+        ]
+      },
+      {
+        path: 'grades',
+        children: [
+          { element: <Navigate to='/dashboard/grades/list' replace />, index: true },
+          { path: 'list', 
+            element: (
+              <RoleBasedGuard hasContent roles={[ROLES.manager, ROLES.teacher]}>
+                <GradesListPage />
+              </RoleBasedGuard>
+          )},
+          {
+            path: 'create',
+            element: (
+              <RoleBasedGuard hasContent roles={[ROLES.manager]}>
+                <GradesCreatePage />
+              </RoleBasedGuard>
+            )
+          },
+        ]
+      },
+      {
+        path: 'degree',
+        children: [
+          { element: <Navigate to='/dashboard/degree/view' replace />, index: true },
+          { path: 'view',
+            element: (
+              <RoleBasedGuard hasContent roles={[ROLES.manager]}>
+                <DegreeViewPage />
+              </RoleBasedGuard>
+            )
+          },
+        ]
+      },
+      {
+        path: 'subjects',
+        children: [
+          { element: <Navigate to='/dashboard/subjects/list' replace />, index: true },
+          { path: 'list', 
+            element: (
+              <RoleBasedGuard hasContent roles={[ROLES.manager, ROLES.teacher]}>
+                <SubjectsListPage />
+              </RoleBasedGuard>
+          )},
+          {
+            path: 'create',
+            element: (
+              <RoleBasedGuard hasContent roles={[ROLES.manager, ROLES.teacher]}>
+                <SubjectsCreatePage />
+              </RoleBasedGuard>
+            )
+          },
+        ]
+      },
+      {
+        path: 'schedule',
+        children: [
+          { element: <Navigate to='/dashboard/schedule/view' replace />, index: true },
+          { path: 'view', 
+            element: (
+              <RoleBasedGuard hasContent roles={[ROLES.teacher]}>
+                <ScheduleViewPage />
+              </RoleBasedGuard>
+          )},
+          {
+            path: 'create',
+            element: (
+              <RoleBasedGuard hasContent roles={[ROLES.teacher]}>
+                <ScheduleCreatePage />
+              </RoleBasedGuard>
+            )
+          },
+        ]
+      },
     ]
   }
-
-
   return useRoutes([
     AuthRoutes,
     DashboardRoutes
