380e6a3618262658c399f1869a6f0c10c0ff19e1
VanessaNicole1 <nicolegualan@gmail.com>
2022-12-19 00:34:55 -0500

    Create page to teachers list

---
+++ src/sections/dashboard/teacher/list/TeacherTableRow.js
@@ -0,0 +1,79 @@
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+import {
+  TableRow,
+  MenuItem,
+  TableCell,
+  IconButton,
+} from '@mui/material';
+
+import Iconify from '../../../../components/iconify';
+import MenuPopover from '../../../../components/menu-popover';
+
+
+TeacherTableRow.propTypes = {
+  row: PropTypes.object,
+  selected: PropTypes.bool,
+  onEditRow: PropTypes.func,
+  onDeleteRow: PropTypes.func,
+  onSelectRow: PropTypes.func,
+};
+
+export default function TeacherTableRow({ row, selected, onEditRow, onSelectRow, onDeleteRow }) {
+  const { user } = row;
+  const [openPopover, setOpenPopover] = useState(null);
+
+  const handleOpenPopover = (event) => {
+    setOpenPopover(event.currentTarget);
+  };
+
+  const handleClosePopover = () => {
+    setOpenPopover(null);
+  };
+
+  return (
+    <>
+      <TableRow hover selected={selected}>
+        <TableCell align="center">{user.name}</TableCell>
+
+        <TableCell align="center" sx={{ textTransform: 'capitalize' }}>
+          {user.lastName}
+        </TableCell>
+        <TableCell align="center">
+          {user.email}
+        </TableCell>
+
+        {/* <TableCell align="left">
+          <Label
+            variant="soft"
+            color={(status === 'banned' && 'error') || 'success'}
+            sx={{ textTransform: 'capitalize' }}
+          >
+            {status}
+          </Label>
+        </TableCell> */}
+
+        <TableCell align="center">
+          <IconButton color={openPopover ? 'inherit' : 'default'} onClick={handleOpenPopover}>
+            <Iconify icon="eva:more-vertical-fill" />
+          </IconButton>
+        </TableCell>
+      </TableRow>
+
+      <MenuPopover
+        open={openPopover}
+        onClose={handleClosePopover}
+        arrow="right-top"
+        sx={{ width: 140 }}
+      >
+
+        <MenuItem
+          onClick={() => {}}
+        >
+          <Iconify icon="ic:baseline-remove-red-eye" />
+          View
+        </MenuItem>
+      </MenuPopover>
+    </>
+  );
+};
---
+++ src/sections/dashboard/teacher/list/TeacherToolbar.js
@@ -0,0 +1,99 @@
+import PropTypes from 'prop-types';
+import { Stack, InputAdornment, TextField, MenuItem, Button } from '@mui/material';
+import Iconify from '../../../../components/iconify';
+import { useLocales } from '../../../../locales';
+
+TeacherToobar.propTypes = {
+  isFiltered: PropTypes.bool,
+  filterContent: PropTypes.string,
+  filterGrade: PropTypes.string,
+  onFilterContent: PropTypes.func,
+  onFilterGrade: PropTypes.func,
+  onResetFilter: PropTypes.func,
+  optionsRole: PropTypes.arrayOf(PropTypes.string),
+};
+
+export default function TeacherToobar({
+  isFiltered,
+  filterContent,
+  filterGrade,
+  optionsRole,
+  onFilterContent,
+  onFilterGrade,
+  onResetFilter,
+}) {
+  const { translate } = useLocales(); 
+
+  return (
+    <Stack
+      spacing={2}
+      alignItems="center"
+      direction={{
+        xs: 'column',
+        sm: 'row',
+      }}
+      sx={{ px: 2.5, py: 3 }}
+    >
+      <TextField
+        fullWidth
+        select
+        label={translate('sections.teacher_toolbar.teacher')}
+        value={filterGrade}
+        onChange={onFilterGrade}
+        SelectProps={{
+          MenuProps: {
+            PaperProps: {
+              sx: {
+                maxHeight: 260,
+              },
+            },
+          },
+        }}
+        sx={{
+          maxWidth: { sm: 240 },
+          textTransform: 'capitalize',
+        }}
+      >
+        {optionsRole.map((option) => (
+          <MenuItem
+            key={option}
+            value={option}
+            sx={{
+              mx: 1,
+              borderRadius: 0.75,
+              typography: 'body2',
+              textTransform: 'capitalize',
+            }}
+          >
+            { option === 'all' ? translate('sections.toolbar_custom.all_option'): option}
+          </MenuItem>
+        ))}
+      </TextField>
+
+      <TextField
+        fullWidth
+        value={filterContent}
+        onChange={onFilterContent}
+        placeholder={translate('sections.toolbar_custom.search')}
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <Iconify icon="eva:search-fill" sx={{ color: 'text.disabled' }} />
+            </InputAdornment>
+          ),
+        }}
+      />
+
+      {isFiltered && (
+        <Button
+          color="error"
+          sx={{ flexShrink: 0 }}
+          onClick={onResetFilter}
+          startIcon={<Iconify icon="eva:trash-2-outline" />}
+        >
+          Clear
+        </Button>
+      )}
+    </Stack>
+  );
+};
