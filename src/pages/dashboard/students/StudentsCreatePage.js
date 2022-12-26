c0b764650be1f6f3062ad742295a701c7d9f0a89
VanessaNicole1 <nicolegualan@gmail.com>
2022-12-26 00:45:57 -0500

    feat: Create Page to create a new student

--- src/pages/dashboard/students/StudentsCreatePage.js
+++ src/pages/dashboard/students/StudentsCreatePage.js
@@ -1,6 +1,39 @@
+import { Helmet } from 'react-helmet-async';
+import { Container } from '@mui/material';
+import { useSettingsContext } from "../../../components/settings";
+import CustomBreadcrumbs from '../../../components/custom-breadcrumbs/CustomBreadcrumbs';
+import { PATH_DASHBOARD } from '../../../routes/paths';
+import StudentNewEditForm from '../../../sections/dashboard/student/create/StudentNewForm';
+import { useLocales } from '../../../locales';
+
 export default function StudentsCreatePage () {
-    return (
-      <h1>Students Create Page</h1>
-    );
-  };
+  const { themeStretch } = useSettingsContext();
+  const { translate } = useLocales();
+
+  return (
+    <>
+      <Helmet>
+        <title> {translate('student_create_page.helmet')} </title>
+      </Helmet>
+
+      <Container maxWidth={themeStretch ? false : 'lg'}>
+        <CustomBreadcrumbs
+          heading={translate('student_create_page.heading')}
+          links={[
+            {
+              name: 'Dashboard',
+              href: PATH_DASHBOARD.root,
+            },
+            {
+              name: translate('student_create_page.link_student'),
+              href: PATH_DASHBOARD.students.listStudents,
+            },
+            { name: translate('student_create_page.link_create_student') },
+          ]}
+        />
+        <StudentNewEditForm />
+      </Container>
+    </>
+  );
+};
   
