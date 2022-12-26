10e5af24d1644a0fe8773c4dedcf0cf57094fa7f
VanessaNicole1 <nicolegualan@gmail.com>
2022-12-26 00:46:32 -0500

    feat: Create Teacher Page

--- src/pages/dashboard/teachers/TeachersCreatePage.js
+++ src/pages/dashboard/teachers/TeachersCreatePage.js
@@ -1,6 +1,40 @@
+import { Helmet } from 'react-helmet-async';
+import { Container } from '@mui/material';
+import { useSettingsContext } from "../../../components/settings";
+import CustomBreadcrumbs from '../../../components/custom-breadcrumbs/CustomBreadcrumbs';
+import { PATH_DASHBOARD } from '../../../routes/paths';
+import TeacherNewEditForm from '../../../sections/dashboard/teacher/create/TeacherNewForm';
+import { useLocales } from '../../../locales';
+
+
 export default function TeachersCreatePage () {
-    return (
-      <h1>Teachers Create Page</h1>
-    );
-  };
+  const { themeStretch } = useSettingsContext();
+  const { translate } = useLocales();
+
+  return (
+    <>
+      <Helmet>
+        <title> {translate('teacher_create_page.helmet')} </title>
+      </Helmet>
+
+      <Container maxWidth={themeStretch ? false : 'lg'}>
+        <CustomBreadcrumbs
+          heading={translate('teacher_create_page.heading')}
+          links={[
+            {
+              name: 'Dashboard',
+              href: PATH_DASHBOARD.root,
+            },
+            {
+              name: translate('teacher_create_page.link_teacher'),
+              href: PATH_DASHBOARD.teachers.listTeachers,
+            },
+            { name: translate('teacher_create_page.link_create_teacher') },
+          ]}
+        />
+        <TeacherNewEditForm />
+      </Container>
+    </>
+  );
+};
   
---
+++ src/sections/dashboard/teacher/create/TeacherNewForm.js
@@ -0,0 +1,134 @@
+import PropTypes from 'prop-types';
+import * as Yup from 'yup';
+import { useCallback, useEffect, useMemo } from 'react';
+import { useNavigate } from 'react-router-dom';
+// form
+import { useForm, Controller } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+// @mui
+import { LoadingButton } from '@mui/lab';
+import { Box, Card, Grid, Stack, Switch, Typography, FormControlLabel } from '@mui/material';
+// utils
+import { useSnackbar } from '../../../../components/snackbar';
+import FormProvider, {
+  RHFSelect,
+  RHFSwitch,
+  RHFTextField,
+  RHFUploadAvatar,
+} from '../../../../components/hook-form';
+import Label from '../../../../components/label';
+import { PATH_DASHBOARD } from '../../../../routes/paths';
+import { createTeacher } from '../../../../services/teacher';
+import { useLocales } from '../../../../locales';
+
+TeacherNewEditForm.propTypes = {
+  isEdit: PropTypes.bool,
+  currentUser: PropTypes.object,
+};
+
+export default function TeacherNewEditForm({ isEdit = false, currentUser }) {
+  const navigate = useNavigate();
+
+  const { enqueueSnackbar } = useSnackbar();
+  const { translate } = useLocales();
+
+
+  const NewUserSchema = Yup.object().shape({
+    name: Yup.string().required(translate('teacher_create_form.name_schema')),
+    lastName: Yup.string().required(translate('teacher_create_form.last_name_schema')),
+    email: Yup.string().required(translate('teacher_create_form.email_schema')).email('Email must be a valid email address'),
+  });
+
+  const defaultValues = useMemo(
+    () => ({
+      name: currentUser?.name || '',
+      lastName: currentUser?.lastname || '',
+      email: currentUser?.email || '',
+    }),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [currentUser]
+  );
+
+  const methods = useForm({
+    resolver: yupResolver(NewUserSchema),
+    defaultValues,
+  });
+
+  const {
+    reset,
+    watch,
+    control,
+    setValue,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = methods;
+
+  const values = watch();
+
+  useEffect(() => {
+    if (isEdit && currentUser) {
+      reset(defaultValues);
+    }
+    if (!isEdit) {
+      reset(defaultValues);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isEdit, currentUser]);
+
+  const onSubmit = async (data) => {
+    try {
+      await createTeacher(data);
+      await new Promise((resolve) => setTimeout(resolve, 500));
+      reset();
+      enqueueSnackbar(!isEdit ? 'Create success!' : 'Update success!');
+      navigate(PATH_DASHBOARD.teachers.listTeachers);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleDrop = useCallback(
+    (acceptedFiles) => {
+      const file = acceptedFiles[0];
+
+      const newFile = Object.assign(file, {
+        preview: URL.createObjectURL(file),
+      });
+
+      if (file) {
+        setValue('avatarUrl', newFile, { shouldValidate: true });
+      }
+    },
+    [setValue]
+  );
+
+  return (
+    <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
+      <Grid container spacing={3}>
+        <Grid item xs={12} md={8}>
+          <Card sx={{ p: 3 }}>
+            <Box
+              rowGap={3}
+              columnGap={2}
+              display="grid"
+              gridTemplateColumns={{
+                xs: 'repeat(1, 1fr)',
+                sm: 'repeat(2, 1fr)',
+              }}
+            >
+              <RHFTextField name="name" label={translate('teacher_create_form.input_name')} />
+              <RHFTextField name="lastName" label={translate('teacher_create_form.last_name_schema')} />
+              <RHFTextField name="email" label={translate('teacher_create_form.email_schema')} />
+            </Box>
+
+            <Stack alignItems="flex-end" sx={{ mt: 3 }}>
+              <LoadingButton type="submit" variant="contained" loading={isSubmitting}>
+                {!isEdit ? translate('teacher_create_form.button_create') : 'Save Changes'}
+              </LoadingButton>
+            </Stack>
+          </Card>
+        </Grid>
+      </Grid>
+    </FormProvider>
+  );
+}
