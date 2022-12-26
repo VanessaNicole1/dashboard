3ddeec91b30d35fa3c00b3fbcaaa7b195e2c433e
VanessaNicole1 <nicolegualan@gmail.com>
2022-12-26 00:47:05 -0500

    feat: Create Degree  Page

---
+++ src/sections/dashboard/degree/create/DegreeNewForm.js
@@ -0,0 +1,109 @@
+import PropTypes from 'prop-types';
+import * as Yup from 'yup';
+import { useCallback, useEffect, useMemo, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { LoadingButton } from '@mui/lab';
+import { Box, Card, Grid, Stack } from '@mui/material';
+import { useSnackbar } from '../../../../components/snackbar';
+import FormProvider, { RHFTextField } from '../../../../components/hook-form';
+import { PATH_DASHBOARD } from '../../../../routes/paths';
+import { createStudent } from '../../../../services/student';
+import { useLocales } from '../../../../locales';
+import { useAuthContext } from '../../../../auth/useAuthContext';
+
+
+DegreeNewEditForm.propTypes = {
+  isEdit: PropTypes.bool,
+  currentUser: PropTypes.object,
+};
+
+export default function DegreeNewEditForm({ isEdit = false, currentUser }) {
+  const { user } = useAuthContext();
+
+  console.log('USER USER', user);
+
+  const { translate } = useLocales();
+  const navigate = useNavigate();
+
+  const { enqueueSnackbar } = useSnackbar();
+
+  const NewUserSchema = Yup.object().shape({
+    name: Yup.string().required(translate('degree_create_form.name_schema')),
+  });
+
+  const defaultValues = useMemo(
+    () => ({
+      name: currentUser?.name || '',
+    }),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [currentUser]
+  );
+
+  const methods = useForm({
+    resolver: yupResolver(NewUserSchema),
+    defaultValues,
+  });
+
+  const {
+    reset,
+    watch,
+    setValue,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = methods;
+
+  const values = watch();
+
+  useEffect(() => {
+    if (isEdit && currentUser) {
+      reset(defaultValues);
+    }
+    if (!isEdit) {
+      reset(defaultValues);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isEdit, currentUser]);
+
+  const onSubmit = async (data) => {
+    try {
+      const message = await createStudent(data);
+      console.log('message', message);
+      await new Promise((resolve) => setTimeout(resolve, 500));
+      reset();
+      enqueueSnackbar(!isEdit ? 'Create success!' : 'Update success!');
+      navigate(PATH_DASHBOARD.degree.view);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
+      <Grid container spacing={3}>
+        <Grid item xs={12} md={8}>
+          <Card sx={{ p: 3 }}>
+            <Box
+              rowGap={3}
+              columnGap={2}
+              display="grid"
+              gridTemplateColumns={{
+                xs: 'repeat(1, 1fr)',
+                sm: 'repeat(2, 1fr)',
+              }}
+            >
+              <RHFTextField name="name" label={translate('degree_create_form.input_name')} />
+            </Box>
+            <Stack alignItems="flex-end" sx={{ mt: 3 }}>
+              <LoadingButton type="submit" variant="contained" loading={isSubmitting}>
+                {!isEdit ? translate('degree_create_form.button_create') : 'Save Changes'}
+              </LoadingButton>
+            </Stack>
+          </Card>
+        </Grid>
+      </Grid>
+    </FormProvider>
+  );
+}
+// {translate('degree_create_page.helmet')}
