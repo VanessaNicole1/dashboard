0e4f0cf606baff085e024c7e51f055ecc4f39a30
VanessaNicole1 <nicolegualan@gmail.com>
2022-12-26 00:47:28 -0500

    feat: Create Role Page

--- src/pages/dashboard/roles/RolesCreatePage.js
+++ src/pages/dashboard/roles/RolesCreatePage.js
@@ -4,29 +4,31 @@ import { PATH_DASHBOARD } from '../../../routes/paths';
 import { useSettingsContext } from '../../../components/settings';
 import CustomBreadcrumbs from '../../../components/custom-breadcrumbs/CustomBreadcrumbs';
 import RoleNewForm from '../../../sections/dashboard/role/create/RoleNewForm';
+import { useLocales } from '../../../locales';
 
 export default function RolesCreatePage() {
   const { themeStretch } = useSettingsContext();
+  const { translate } = useLocales();
 
   return (
     <>
       <Helmet>
-        <title> User: Create a new role</title>
+        <title> {translate('role_create_page.helmet')} </title>
       </Helmet>
 
       <Container maxWidth={themeStretch ? false : 'lg'}>
         <CustomBreadcrumbs
-          heading="Create a new role"
+          heading={ translate('role_create_page.heading') }
           links={[
             {
               name: 'Dashboard',
               href: PATH_DASHBOARD.root,
             },
             {
-              name: 'Role',
+              name: translate('role_create_page.link_degree'),
               href: PATH_DASHBOARD.roles.listRoles,
             },
-            { name: 'New role' },
+            { name: translate('role_create_page.link_create_degree') },
           ]}
         />
         <RoleNewForm />
