2bf6a305d4518e47fe271637839131693b218bd6
VanessaNicole1 <nicolegualan@gmail.com>
2023-04-09 19:55:05 -0500

    Update CreateGeneralInformationForm.js
    Several UI improvements.

--- src/sections/dashboard/lesson-plan/start-process/general-information/CreateGeneralInformationForm.js
+++ src/sections/dashboard/lesson-plan/start-process/general-information/CreateGeneralInformationForm.js
@@ -9,15 +9,7 @@ import { LoadingButton } from '@mui/lab';
 import FormProvider from '../../../../../components/hook-form/FormProvider';
 import { RHFSelect, RHFTextField } from '../../../../../components/hook-form';
 import { getUsersWithManagerRole } from '../../../../../services/user';
-
-const schemaErrorMessages = {
-  requiredStartDate: 'La fecha de inicio del periodo es requerida.',
-  requiredEndDate: 'La fecha de fin del periodo es requerida.',
-  endDateShouldBeGreater:
-    'La fecha de fin de periodo debe ser mayor a la fecha de inicio de periodo.',
-  requiredManager: 'El director de carrera es requerido.',
-  requiredDegree: 'La carrera es requerida.',
-};
+import useLocales from '../../../../../locales/useLocales';
 
 CreateGeneralInformationForm.propTypes = {
   onFillGeneralInformation: PropTypes.func,
@@ -33,6 +25,11 @@ export default function CreateGeneralInformationForm({
   onFillGeneralInformation,
   generalInformation,
 }) {
+
+  const { translate } = useLocales();
+  const i18nGeneralInformationKey = 'lesson_plan.start_process.general_information';
+  const i18nFormKey = `${i18nGeneralInformationKey}.form`;
+  const i18nErrorsKey = `${i18nGeneralInformationKey}.errors`;
   const OnedayInMiliseconds = 86400000;
   const [managers, setManagers] = useState([]);
 
@@ -45,6 +42,14 @@ export default function CreateGeneralInformationForm({
     fetchManagers();
   }, []);
 
+  const schemaErrorMessages = {
+    requiredStartDate: translate(`${i18nErrorsKey}.required_start_date`),
+    requiredEndDate: translate(`${i18nErrorsKey}.required_end_date`),
+    endDateShouldBeGreater: translate(`${i18nErrorsKey}.greater_end_date`),
+    requiredManager: translate(`${i18nErrorsKey}.required_manager`),
+    requiredDegree: translate(`${i18nErrorsKey}.required_degree`),
+  };
+  
   const generalInformationSchema = Yup.object().shape({
     startDate: Yup.date()
       .required(schemaErrorMessages.requiredStartDate)
@@ -90,7 +95,7 @@ export default function CreateGeneralInformationForm({
   return (
     <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
       <Divider sx={{ marginBottom: 3 }}>
-        <Chip label='Periodo' />
+        <Chip label={translate(`${i18nGeneralInformationKey}.period_chip`)} />
       </Divider>
 
       <Stack spacing={2} direction={{ xs: 'column', sm: 'row' }}>
@@ -99,7 +104,7 @@ export default function CreateGeneralInformationForm({
           control={control}
           render={({ field, fieldState: { error } }) => (
             <DatePicker
-              label='Inicio del Periodo'
+              label={translate(`${i18nFormKey}.period.start_date`)}
               value={field.value}
               onChange={(newValue) => {
                 field.onChange(newValue);
@@ -121,7 +126,7 @@ export default function CreateGeneralInformationForm({
           control={control}
           render={({ field, fieldState: { error } }) => (
             <DatePicker
-              label='Fin del Periodo'
+              label={translate(`${i18nFormKey}.period.end_date`)}
               value={field.value}
               onChange={(newValue) => {
                 field.onChange(newValue);
@@ -140,19 +145,19 @@ export default function CreateGeneralInformationForm({
       </Stack>
 
       <Divider sx={{ marginBottom: 3, marginTop: 3 }}>
-        <Chip label='Carrera' />
+        <Chip label={translate(`${i18nGeneralInformationKey}.degree_chip`)} />
       </Divider>
 
-      <RHFTextField name='degree' label='Nombre de la carrera' />
+      <RHFTextField name='degree' label={translate(`${i18nFormKey}.degree`)} />
 
       <Divider sx={{ marginBottom: 3, marginTop: 3 }}>
-        <Chip label='Director de Carrera' />
+        <Chip label={translate(`${i18nGeneralInformationKey}.manager_chip`)} />
       </Divider>
 
       <RHFSelect
         native
         name='manager'
-        label='Seleccione el director de carrera:'
+        label={translate(`${i18nFormKey}.manager`)}
       >
         <option value='' />
         {managers.map((manager) => (
@@ -164,7 +169,7 @@ export default function CreateGeneralInformationForm({
 
       <Stack alignItems='flex-end' sx={{ mt: 3 }}>
         <LoadingButton type='submit' variant='contained' loading={isSubmitting}>
-          Siguiente Paso
+          { translate(`${i18nGeneralInformationKey}.next_step_button`) }
         </LoadingButton>
       </Stack>
     </FormProvider>
