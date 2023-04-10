68e30146e4493273d04bcd1425e3b5e56da96129
VanessaNicole1 <nicolegualan@gmail.com>
2023-04-09 19:57:23 -0500

    Update CreateTeachers.js

--- src/sections/dashboard/lesson-plan/start-process/teachers/CreateTeachers.js
+++ src/sections/dashboard/lesson-plan/start-process/teachers/CreateTeachers.js
@@ -1,29 +1,52 @@
 import PropTypes from 'prop-types';
-import { Grid, Button } from '@mui/material';
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  Typography,
+} from '@mui/material';
+import CreateEntitiesForm from '../../common/CreateEntitiesForm';
+import { validateTeachers } from '../../../../../services/teacher';
+import { useLocales } from '../../../../../locales';
 
 CreateTeachers.propTypes = {
-  checkout: PropTypes.object,
-  onNextStep: PropTypes.func,
+  csv: PropTypes.any,
+  teachers: PropTypes.array,
+  onBackStep: PropTypes.func,
+  onCreateTeachers: PropTypes.func,
 };
 
-export default function CreateTeachers({
-  checkout,
-  onNextStep,
-}) {
-//   const { cart, total, discount, subtotal } = checkout;
+export default function CreateTeachers({ csv, teachers, onBackStep, onCreateTeachers }) {
+  const { translate } = useLocales(); 
+
+  const csvTeachersHeaders = {
+    Nombre: 'name',
+    Apellido: 'lastName',
+    Correo: 'email',
+    Materia: 'subject',
+    Ciclo: 'numberParallel',
+    Paralelo: 'parallel',
+  };
+
+  const tableHeaders = ['name', 'lastname', 'email', 'subject', 'grade', 'actions'];
 
   return (
-    <Grid container spacing={3}>
-      <h1>Students</h1>
-      <Button
-        fullWidth
-        size="large"
-        type="submit"
-        variant="contained"
-        onClick={onNextStep}
-      >
-      Check Out
-      </Button>
-    </Grid>
+    <Card>
+      <CardHeader
+        title={<Typography variant='h4'>{ translate('lesson_plan.start_process.teachers.header_title') }</Typography>}
+      />
+      <CardContent>
+        <CreateEntitiesForm
+          csv={csv}
+          entitiesData={teachers}
+          onBackStep={onBackStep}
+          onCreateEntities={onCreateTeachers}
+          validateEntities={validateTeachers}
+          csvHeaders={csvTeachersHeaders}
+          entity='teachers'
+          tableHeaders={tableHeaders}
+        />
+      </CardContent>
+    </Card>
   );
 }
