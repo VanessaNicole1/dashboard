b25c6fcba3c31081774568f85c01e4259a03b044
VanessaNicole1 <nicolegualan@gmail.com>
2023-04-09 19:55:10 -0500

    Update FillGeneralInformation.js

--- src/sections/dashboard/lesson-plan/start-process/general-information/FillGeneralInformation.js
+++ src/sections/dashboard/lesson-plan/start-process/general-information/FillGeneralInformation.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import { Grid, CardHeader, Typography, CardContent, Card } from '@mui/material';
 import CreateGeneralInformationForm from './CreateGeneralInformationForm';
+import useLocales from '../../../../../locales/useLocales';
 
 FillGeneralInformation.propTypes = {
   onFillGeneralInformation: PropTypes.func,
@@ -16,12 +17,19 @@ export default function FillGeneralInformation({
   generalInformation,
   onFillGeneralInformation,
 }) {
+
+  const { translate } = useLocales();
+
   return (
     <Grid container spacing={3}>
       <Grid item xs={12} md={12} lg={12}>
         <Card sx={{ mb: 3 }} style={{ paddingBottom: 40 }}>
           <CardHeader
-            title={<Typography variant='h4'>General Information</Typography>}
+            title={
+              <Typography variant='h4'>
+                { translate('lesson_plan.start_process.general_information.title') }
+              </Typography>
+            }
           />
 
           <CardContent>
