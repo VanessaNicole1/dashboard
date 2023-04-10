82190d7c3689719efc14591fe3d54a2ff95b2941
VanessaNicole1 <nicolegualan@gmail.com>
2023-04-09 19:50:57 -0500

    Add i18n support.

--- src/locales/langs/en.js
+++ src/locales/langs/en.js
@@ -15,197 +15,344 @@ const en = {
     description: `Need help? \n Please check our docs.`,
     documentation: `documentation`,
   },
-  lesson_plan_list_page: {
-    helmet: 'Lesson Plan: List',
-    heading: 'Lesson Plans List',
-    dashboard: 'Dashboard',
-    lesson_plan: 'Lesson Plan',
-    list: 'List',
-    table: {
-      created_date: 'Created Date',
-      grade: 'Grade',
-      teacher: 'Teacher',
-      subject: 'Subject',
-      actions: 'Actions'
+  services: {
+    teachers: {
+      validateTeachers: {
+        successful: 'Teachers CSV file is correctly structured.'
+      }
+    },
+    students: {
+      validateStudents: {
+        successful: 'Students CSV file is correctly structured.'
+      }
     }
   },
+  lesson_plan: {
+    start_process: {
+      helmet: 'Lesson Plan - Start Process',
+      breadcrumbs: {
+        title: 'Start Process',
+        lesson_plan: 'Lesson Plan',
+        start_process: 'Start Process'
+      },
+      steps: {
+        general_information: 'General Information',
+        students: 'Students',
+        teachers: 'Teachers',
+        review_information: 'Review Information'
+      },
+      csv_errors: {
+        has_errors: "CSV file has the following errors:",
+        min_required: "CSV file is required",
+        max_accepted: "Only one CSV file is allowed",
+        empty: "CVS file does not contain records",
+      },
+      common: {
+        back_button: "Back Step",
+        next_button: "Next Step",
+        create: "Create {{entity}}",
+        alert: "CSV file format should be as follows:",
+        validation_error: "The following errors have been found:",
+        invalid_field:
+          'The "{{header}}" field is not part of the CSV file format.',
+        required_headers_amount: "The CSV file should have {{amount}} fields:",
+        remove_csv_files: "Delete File",
+        view_content: "View CSV Content",
+        confirm_dialog:
+          "Are you sure to remove <strong>{{amount}}</strong> records?",
+        confirm_dialog_title: "Delete",
+        form: {
+          name: "Name",
+          last_name: "Lastname",
+          subject: "Subject",
+          email: "Email",
+          number_parallel: "Cycle",
+          parallel: "Parallel",
+          buttons: {
+            cancel: "Cancel",
+            edit: "Edit",
+            create: "Create"
+          },
+          errors: {
+            min_name: "{{entity}} name should have at least 3 characters",
+            required_name: "{{entity}} name is required.",
+            min_lastname:
+              "{{entity}} last name should have at least 3 characters",
+            required_lastname: "{{entity}} last name is required.",
+            invalid_email: "Make sure it is a valid email.",
+            required_email: "{{entity}} email is required.",
+            max_number_parallel:
+              "Cycle should not have more than 2 characters.",
+            required_number_parallel: "{{entity}} cycle is required.",
+            only_numbers: 'Cycle should have just numeric values.',
+            max_parallel: "Parallel should not have more than 1 character.",
+            required_parallel: "{{entity}} parallel is required.",
+            min_subject: "Subject should have at least 3 characters.",
+            required_subject: "{{entity}} subject is required.",
+          },
+        },
+        table: {
+          tooltip: "Delete",
+          row: {
+            tooltip: "Edit {{email}}",
+          },
+          headers: {
+            name: "Name",
+            lastname: "Last Name",
+            email: "Email",
+            subject: "Subject",
+            grade: "Grade",
+            actions: "Actions",
+          },
+        },
+      },
+      general_information: {
+        title: "General Information",
+        period_chip: "Period",
+        degree_chip: "Degree",
+        manager_chip: "Manager",
+        next_step_button: "Next Step",
+        form: {
+          period: {
+            start_date: "Start of Period",
+            end_date: "End of Period",
+          },
+          degree: "Degree name",
+          manager: "Select degree manager",
+        },
+        errors: {
+          required_start_date: "Period start date is required.",
+          required_end_date: "Period end date is required.",
+          greater_end_date:
+            "Period end date should be greater than period start date",
+          required_manager: "Manager is required.",
+          required_degree: "Degree is required.",
+        },
+      },
+      students: {
+        entity: "Student",
+        header_title: "Students",
+        info: "Please provide students CSV file",
+        required_entity: "Students are required to continue with the process.",
+        dialog_title_edit: "Edit Student",
+        dialog_title_create: "Create Student",
+      },
+      teachers: {
+        entity: "Teacher",
+        header_title: "Teachers",
+        info: "Please provide teachers CSV file",
+        required_entity: "Teachers are required to continue with the process.",
+        dialog_title_edit: "Edit Teacher",
+        dialog_title_create: "Create Teacher",
+      },
+      review_information: {
+        title: "Summary",
+        description:
+          "The following is a summary of all the information in order to start the process, to verify the information you can select each of the corresponding sections.",
+        period_budge: {
+          title: "Period",
+          start: "Start",
+          end: "End"
+        },
+        manager: 'Manager',
+        degree: 'Degree',
+        students: 'Students',
+        teachers: 'Teachers',
+        next_step_button: 'Start Process',
+        back_step_button: 'Back Step',
+        alert_message: 'Please, solve the errors to continue with the process.'
+      },
+    },
+  },
+  lesson_plan_list_page: {
+    helmet: "Lesson Plan: List",
+    heading: "Lesson Plans List",
+    dashboard: "Dashboard",
+    lesson_plan: "Lesson Plan",
+    list: "List",
+    table: {
+      created_date: "Created Date",
+      grade: "Grade",
+      teacher: "Teacher",
+      subject: "Subject",
+      actions: "Actions",
+    },
+  },
   degree_create_page: {
-    helmet: 'Degree: Create a new degree',
-    heading: 'Create a new degree',
-    link_degree: 'Degree',
-    link_create_degree: 'New degree'
-
+    helmet: "Degree: Create a new degree",
+    heading: "Create a new degree",
+    link_degree: "Degree",
+    link_create_degree: "New degree",
   },
   role_create_page: {
-    helmet: 'Role: Create a new role',
-    heading: 'Create a new role',
-    link_degree: 'Role',
-    link_create_degree: 'New role'
-
+    helmet: "Role: Create a new role",
+    heading: "Create a new role",
+    link_degree: "Role",
+    link_create_degree: "New role",
   },
   student_create_page: {
-    helmet: 'Student: Create a new estudent',
-    heading: 'Create a new student',
-    link_student: 'Student',
-    link_create_student: 'New student'
-
+    helmet: "Student: Create a new estudent",
+    heading: "Create a new student",
+    link_student: "Student",
+    link_create_student: "New student",
   },
   teacher_create_page: {
-    helmet: 'Teacher: Create a new teacher',
-    heading: 'Create a new teacher',
-    link_teacher: 'Teacher',
-    link_create_teacher: 'New teacher'
-
+    helmet: "Teacher: Create a new teacher",
+    heading: "Create a new teacher",
+    link_teacher: "Teacher",
+    link_create_teacher: "New teacher",
   },
   components: {
     table_pagination_custom: {
       form_control: {
-        label: 'Dense'
-      }
-    }
+        label: "Dense",
+      },
+    },
+    table: {
+      selected_action: "{{number}} selected",
+    },
   },
   sections: {
     toolbar_custom: {
-      search: 'Search...',
-      all_option: 'All'
+      search: "Search...",
+      all_option: "All",
     },
     lesson_plan_toolbar: {
-      grade: 'Grade',
+      grade: "Grade",
     },
     role_toolbar: {
-      type: 'Type'
+      type: "Type",
     },
     teacher_toolbar: {
-      teacher: 'Teacher',
+      teacher: "Teacher",
     },
     student_toolbar: {
-      grade: 'Grade',
+      grade: "Grade",
     },
     grade_toolbar: {
-      degree: 'Degree',
+      degree: "Degree",
     },
     degree_toolbar: {
-      degree: 'Degree'
+      degree: "Degree",
     },
     students: {
       schema: {
-        name: 'Name is required'
-      }
-    }
+        name: "Name is required",
+      },
+    },
   },
-  lesson_plan: 'lesson plan',
-  about: 'about',
-  start_process: 'start process',
-  all_plans: 'all plans',
+  about: "about",
+  start_process: "start process",
+  all_plans: "all plans",
   teacher_plans: `teacher's plans`,
-  create: 'create',
-  list: 'list',
-  pending: 'pending',
+  create: "create",
+  list: "list",
+  pending: "pending",
   roles_list_page: {
-    helmet: 'Roles | List',
-    heading: 'Role List',
-    dashboard: 'Dashboard',
-    roles: 'Roles',
-    list: 'List',
+    helmet: "Roles | List",
+    heading: "Role List",
+    dashboard: "Dashboard",
+    roles: "Roles",
+    list: "List",
     table: {
-      id: 'ID',
-      type: 'Type',
-      actions: 'Actions'
-    }
+      id: "ID",
+      type: "Type",
+      actions: "Actions",
+    },
   },
   students_list_page: {
-    helmet: 'Students | List',
-    heading: 'Students List',
-    dashboard: 'Dashboard',
-    roles: 'Students',
-    list: 'List',
+    helmet: "Students | List",
+    heading: "Students List",
+    dashboard: "Dashboard",
+    roles: "Students",
+    list: "List",
     table: {
-      name: 'Name',
-      last: 'Last Name',
-      email: 'Email',
-      actions: 'Actions',
-      grade: 'Grade'
-    }
+      name: "Name",
+      last: "Last Name",
+      email: "Email",
+      actions: "Actions",
+      grade: "Grade",
+    },
   },
   teachers_list_page: {
-    helmet: 'Teachers | List',
-    heading: 'Teachers List',
-    dashboard: 'Dashboard',
-    teachers: 'Teachers',
-    list: 'List',
+    helmet: "Teachers | List",
+    heading: "Teachers List",
+    dashboard: "Dashboard",
+    teachers: "Teachers",
+    list: "List",
     table: {
-      name: 'Name',
-      last: 'Last Name',
-      email: 'Email',
-      actions: 'Actions',
-    }
+      name: "Name",
+      last: "Last Name",
+      email: "Email",
+      actions: "Actions",
+    },
   },
   grades_list_page: {
-    helmet: 'Grades | List',
-    heading: 'Grades List',
-    dashboard: 'Dashboard',
-    grades: 'Grades',
-    list: 'List',
+    helmet: "Grades | List",
+    heading: "Grades List",
+    dashboard: "Dashboard",
+    grades: "Grades",
+    list: "List",
     table: {
-      grade: 'Grade',
-      parallel: 'Parallel',
-      degree: 'Degree',
-      actions: 'Actions',
-    }
+      grade: "Grade",
+      parallel: "Parallel",
+      degree: "Degree",
+      actions: "Actions",
+    },
   },
   subjects_list_page: {
-    helmet: 'Subjects | List',
-    heading: 'Subjects List',
-    dashboard: 'Dashboard',
-    subjects: 'Subjects',
-    list: 'List',
+    helmet: "Subjects | List",
+    heading: "Subjects List",
+    dashboard: "Dashboard",
+    subjects: "Subjects",
+    list: "List",
     table: {
-      id: 'ID',
-      name: 'Name',
-      actions: 'Actions',
-    }
+      id: "ID",
+      name: "Name",
+      actions: "Actions",
+    },
   },
   degrees_list_page: {
-    helmet: 'Degrees | List',
-    heading: 'Degrees List',
-    dashboard: 'Dashboard',
-    degrees: 'Degrees',
-    list: 'List',
+    helmet: "Degrees | List",
+    heading: "Degrees List",
+    dashboard: "Dashboard",
+    degrees: "Degrees",
+    list: "List",
     table: {
-      period: 'Period',
-      name: 'Name',
-      actions: 'Actions',
-    }
+      period: "Period",
+      name: "Name",
+      actions: "Actions",
+    },
   },
   degree_create_form: {
-    button_create: 'Create degree',
-    input_name: 'Degree name',
-    name_schema: 'Name is required'
+    button_create: "Create degree",
+    input_name: "Degree name",
+    name_schema: "Name is required",
   },
   role_create_form: {
-    button_create: 'Create role',
-    input_name: 'Role type',
-    role_schema: 'Type is required'
+    button_create: "Create role",
+    input_name: "Role type",
+    role_schema: "Type is required",
   },
   student_create_form: {
-    button_create: 'Create student',
-    name_schema: 'Name is required',
-    last_name_schema: 'Last Name is required',
-    email_schema: 'Email is required',
-    grade_schema: 'Grade is required',
-    input_name: 'Name',
-    last_name_input: 'Last Name',
-    email_input: 'Email',
-    grade_input: 'Grade'
+    button_create: "Create student",
+    name_schema: "Name is required",
+    last_name_schema: "Last Name is required",
+    email_schema: "Email is required",
+    grade_schema: "Grade is required",
+    input_name: "Name",
+    last_name_input: "Last Name",
+    email_input: "Email",
+    grade_input: "Grade",
   },
   teacher_create_form: {
-    button_create: 'Create teacher',
-    name_schema: 'Name is required',
-    last_name_schema: 'Last Name is required',
-    email_schema: 'Email is required',
-    input_name: 'Name',
-    last_name_input: 'Last Name',
-    email_input: 'Email',
+    button_create: "Create teacher",
+    name_schema: "Name is required",
+    last_name_schema: "Last Name is required",
+    email_schema: "Email is required",
+    input_name: "Name",
+    last_name_input: "Last Name",
+    email_input: "Email",
   },
   app: `app`,
   user: `user`,
--- src/locales/langs/es.js
+++ src/locales/langs/es.js
@@ -15,6 +15,151 @@ const en = {
     description: `Need help? \n Please check our docs.`,
     documentation: `documentation`,
   },
+  services: {
+    teachers: {
+      validateTeachers: {
+        successful: 'El archivo CSV de docentes está correctamente estructurado.'
+      }
+    },
+    students: {
+      validateStudents: {
+        successful: 'El archivo CSV de estudiantes está correctamente estructurado.'
+      }
+    }
+  },
+  lesson_plan: {
+    start_process: {
+      helmet: 'Plan de clases - Iniciar Proceso',
+      breadcrumbs: {
+        title: 'Iniciar Proceso',
+        lesson_plan: 'Plan de Clases',
+        start_process: 'Iniciar Proceso'
+      },
+      steps: {
+        general_information: 'Información General',
+        students: 'Estudiantes',
+        teachers: 'Docentes',
+        review_information: 'Revisar Información'
+      },
+      csv_errors: {
+        has_errors: 'El archivo CSV contiene los siguientes errores:',
+        min_required: 'El archivo CSV es requerido.',
+        max_accepted: 'Solamente es aceptado un archivo CSV.',
+        empty: 'El archivo CSV no contiene registros.',
+      },
+      common: {
+        back_button: 'Paso Anterior',
+        next_button: 'Siguiente Paso',
+        create: "Crear {{entity}}",
+        alert: 'El formato del archivo csv debe ser el siguiente:',
+        validation_error: 'Se han encontrado los siguientes errores:',
+        invalid_field:
+          'El campo "{{header}}" no forma parte del formato del archivo CSV.',
+        required_headers_amount: 'El archivo CSV debe tener {{amount}} campos:',
+        remove_csv_files: 'Eliminar archivo',
+        view_content: 'Visualizar contenido',
+        confirm_dialog: '¿Está seguro de eliminar <strong>{{amount}}</strong> registros?',
+        confirm_dialog_title: 'Eliminar',
+        form: {
+          name: 'Nombre',
+          last_name: 'Apellido',
+          subject: 'Materia',
+          email: 'Correo electrónico',
+          number_parallel: 'Ciclo',
+          parallel: 'Paralelo',
+          buttons: {
+            cancel: 'Cancelar',
+            edit: 'Editar',
+            create: "Crear"
+          },
+          errors: {
+            min_name: 'El nombre del {{entity}} debe tener al menos 3 caracteres',
+            required_name: 'El nombre del {{entity}} es requerido.',
+            min_lastname: 'El apellido del {{entity}} debe tener al menos 3 caracteres',
+            required_lastname: 'El apellido del {{entity}} es requerido.',
+            invalid_email: 'Asegúrese que sea un correo válido.',
+            required_email: 'El email del {{entity}} es requerido.',
+            max_number_parallel: 'El ciclo no puede tener más de dos caracteres',
+            required_number_parallel: 'El ciclo del {{entity}} es requerido.',
+            only_numbers: 'El ciclo debe tener solamente valores númericos',
+            max_parallel: 'El paralelo no puede tener más de un carácter',
+            required_parallel: 'El paralelo del {{entity}} es requerido.',
+            min_subject: 'La materia debe tener al menos 3 caracteres',
+            required_subject: 'La materia del {{entity}} es requerida'
+          }
+        },
+        table: {
+          tooltip: 'Eliminar',
+          row: {
+            tooltip: 'Editar {{email}}'
+          },
+          headers: {
+            name: 'Nombre',
+            lastname: 'Apellidos',
+            email: 'Correo Electrónico',
+            subject: 'Materia',
+            grade: 'Ciclo',
+            actions: 'Acciones'
+          }
+        }
+      },
+      general_information: {
+        title: 'Información General',
+        period_chip: 'Periodo',
+        degree_chip: 'Carrera',
+        manager_chip: 'Director de Carrera',
+        next_step_button: 'Siguiente Paso',
+        form: {
+          period: {
+            start_date: 'Inicio del Periodo',
+            end_date: 'Fin del periodo'
+          },
+          degree: 'Nombre de la Carrera',
+          manager: 'Seleccione el director de carrera'
+        },
+        errors: {  
+          required_start_date: 'La fecha de inicio del periodo es requerida.',
+          required_end_date: 'La fecha de fin del periodo es requerida',
+          greater_end_date: 'La fecha de fin de periodo debe ser mayor a la fecha de inicio de periodo',
+          required_manager: 'El director de carrera es requerido.',
+          required_degree: 'La carrera es requerida.'
+        }
+      },
+      students: {
+        entity: 'Estudiante',
+        header_title: 'Estudiantes',
+        info: 'Por favor proporcione el archivo CSV de estudiantes',
+        required_entity: 'Los estudiantes son requeridos para continuar con el proceso.',
+        dialog_title_edit: 'Editar Estudiante',
+        dialog_title_create: 'Crear Estudiante'
+      },
+      teachers: {
+        entity: 'Docente',
+        header_title: 'Docentes',
+        info: 'Por favor proporcione el archivo CSV de docentes',
+        required_entity: 'Los docentes son requeridos para continuar con el proceso.',
+        dialog_title_edit: 'Editar Docente',
+        dialog_title_create: 'Crear Docente'
+      },
+      review_information: {
+        title: "Resumen",
+        description:
+          "A continuación se presenta un resumen de toda la información para el inicio del proceso, para verificar la información puede seleccionar cada uno de los apartados correspondientes.",
+        period_budge: {
+          title: "Periodo",
+          start: "Inicio",
+          end: "Fin"
+        },
+        manager: 'Director de Carrera',
+        degree: 'Carrera',
+        students: 'Estudiantes',
+        teachers: 'Docentes',
+        next_step_button: 'Iniciar Proceso',
+        back_step_button: 'Paso Anterior',
+        alert_message: 'Por favor, solucione los errores para continuar con el proceso.'
+      }
+    },
+  },
   lesson_plan_list_page: {
     helmet: 'Plan de Clases | Lista',
     heading: 'Listado de Plan de Clases',
@@ -26,51 +171,50 @@ const en = {
       grade: 'Curso',
       teacher: 'Docente',
       subject: 'Materia',
-      actions: 'Acciones'
-    }
+      actions: 'Acciones',
+    },
   },
   degree_create_page: {
     helmet: 'Carrera: Crear una nueva carrera',
     heading: 'Crear una nueva carrera',
     link_degree: 'Carrera',
-    link_create_degree: 'Nueva Carrera'
-
+    link_create_degree: 'Nueva Carrera',
   },
   role_create_page: {
     helmet: 'Rol: Crear una nuevo rol',
     heading: 'Crear un nuevo rol',
     link_degree: 'Rol',
-    link_create_degree: 'Nuevo rol'
-
+    link_create_degree: 'Nuevo rol',
   },
   student_create_page: {
     helmet: 'Estudiante: Crear un nuevo estudiante',
     heading: 'Crear un nuevo estudiante',
     link_student: 'Estudiante',
-    link_create_student: 'Nuevo estudiante'
-
+    link_create_student: 'Nuevo estudiante',
   },
   teacher_create_page: {
     helmet: 'Docente: Crear nuevo docente',
     heading: 'Crear un nuevo docente',
     link_teacher: 'Docente',
-    link_create_teacher: 'Docente nuevo'
-
+    link_create_teacher: 'Docente nuevo',
   },
   components: {
     table_pagination_custom: {
       form_control: {
-        label: 'Densidad'
-      }
+        label: 'Densidad',
+      },
+    },
+    table: {
+      selected_action: '{{number}} seleccionados'
     }
   },
   sections: {
     toolbar_custom: {
       search: 'Buscar...',
-      all_option: 'Todos'
+      all_option: 'Todos',
     },
     role_toolbar: {
-      type: 'Tipo'
+      type: 'Tipo',
     },
     lesson_plan_toolbar: {
       grade: 'Curso',
@@ -86,11 +230,10 @@ const en = {
     },
     students: {
       schema: {
-        name: 'El nombre es requerido'
-      }
-    }
+        name: 'El nombre es requerido',
+      },
+    },
   },
-  lesson_plan: 'plan de clases',
   about: 'información',
   start_process: 'iniciar proceso',
   all_plans: 'planes de clase',
@@ -107,8 +250,8 @@ const en = {
     table: {
       id: 'ID',
       type: 'Tipo',
-      actions: 'Acciones'
-    }
+      actions: 'Acciones',
+    },
   },
   students_list_page: {
     helmet: 'Estudiantes | Lista',
@@ -121,8 +264,8 @@ const en = {
       last: 'Apellido',
       email: 'Correo Electrónico',
       actions: 'Acciones',
-      grade: 'Curso'
-    }
+      grade: 'Curso',
+    },
   },
   teachers_list_page: {
     helmet: 'Docentes | Lista',
@@ -135,7 +278,7 @@ const en = {
       last: 'Apellido',
       email: 'Correo Electrónico',
       actions: 'Acciones',
-    }
+    },
   },
   grades_list_page: {
     helmet: 'Cursos | Lista',
@@ -148,7 +291,7 @@ const en = {
       parallel: 'Paralelo',
       degree: 'Carrera',
       actions: 'Acciones',
-    }
+    },
   },
   degrees_list_page: {
     helmet: 'Carreras | Lista',
@@ -160,17 +303,17 @@ const en = {
       period: 'Periodo',
       name: 'Nombre',
       actions: 'Acciones',
-    }
+    },
   },
   degree_create_form: {
     button_create: 'Crear Carrera',
     input_name: 'Nombre de la carrera',
-    name_schema: 'El nombre es requerido'
+    name_schema: 'El nombre es requerido',
   },
   role_create_form: {
     button_create: 'Crear rol',
     input_name: 'Tipo de rol',
-    role_schema: 'El rol es requerido'
+    role_schema: 'El rol es requerido',
   },
   student_create_form: {
     button_create: 'Crear estudiante',
@@ -181,7 +324,7 @@ const en = {
     input_name: 'Nombre',
     last_name_input: 'Apellido',
     email_input: 'Correo',
-    grade_input: 'Curso'
+    grade_input: 'Curso',
   },
   teacher_create_form: {
     button_create: 'Crear docente',
