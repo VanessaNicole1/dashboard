a68526be3d2fd27f16682a5cbf082b6e69153a86
VanessaNicole1 <nicolegualan@gmail.com>
2023-04-12 20:16:38 -0500

    feat: Update degree service

--- src/services/degree.js
+++ src/services/degree.js
@@ -2,15 +2,14 @@ import axios from "../utils/axios";
 
 
 export const createDegree = async (data) => {
-    try {
-      const { name } = data;
-      const post = { name };
-      const res = await axios.post('/degree', post);
-      return res.data;
-    } catch (error) {
-      console.log('ERRORRRRR', error);
-      return {
-        message: "Couldn't create a new student"
-      }
+  try {
+    const { name } = data;
+    const post = { name };
+    const res = await axios.post('/degree', post);
+    return res.data;
+  } catch (error) {
+    return {
+      message: "Couldn't create a new student"
     }
   }
+};
