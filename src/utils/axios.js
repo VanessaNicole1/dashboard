d56967c0efc8792c405f93f46b3d8cb7454ee827
VanessaNicole1 <nicolegualan@gmail.com>
2023-04-13 23:48:01 -0500

    Update axios.js
    Add header for i18n for every request.

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,11 +1,19 @@
 import axios from 'axios';
 import { HOST_API_KEY } from '../config-global';
+import i18n from '../locales/i18n';
 
 const axiosInstance = axios.create({ baseURL: HOST_API_KEY });
 
 axiosInstance.interceptors.response.use(
   (response) => response,
   (error) => Promise.reject((error.response && error.response.data) || 'Something went wrong')
+);
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    config.headers['Accept-Language'] = i18n.language;
+    return config;
+  }
 );
 
 export default axiosInstance;
