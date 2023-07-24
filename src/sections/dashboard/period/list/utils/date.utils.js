50b74659679e3e24c0518440fe369e53947ade4a
VanessaNicole1 <nicolegualan@gmail.com>
2023-07-24 00:13:14 -0500

    feat: Improve date

--- src/sections/dashboard/period/list/utils/date.utils.js
+++ src/sections/dashboard/period/list/utils/date.utils.js
@@ -1,3 +1,6 @@
 export const getFullYears = (date) => new Date(date).getFullYear()
 
-export const getMonth = (date) => new Date(date).toLocaleString('default', { month: 'long' });
+export const getMonth = (date) => { 
+    const monthName = new Date(date).toLocaleDateString('es-ES', { month: 'long' });
+    return monthName.charAt(0).toUpperCase() + monthName.slice(1);
+}
