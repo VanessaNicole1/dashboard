1abbec7a0203e0efaabe4abbe3b0c32b1ead40f3
nicolegualan@gmail.com <VanessaNicole1>
2023-08-14 23:42:32 -0500

    Improve Reports Page

--- src/components/custom-avatar/CustomAvatar.js
+++ src/components/custom-avatar/CustomAvatar.js
@@ -3,23 +3,29 @@ import { forwardRef } from 'react';
 import { useTheme } from '@mui/material/styles';
 import { Badge, Avatar } from '@mui/material';
 
-const getCharAtName = (name) => name && name.charAt(0).toUpperCase();
+const getCharAtName = (name, custom = undefined) => {
+  if (!custom) {
+    return name && name.charAt(0).toUpperCase();
+  }
 
-const getColorByName = (name) => {
-  if (['A', 'N', 'H', 'L', 'Q'].includes(getCharAtName(name))) return 'primary';
-  if (['F', 'G', 'T', 'I', 'J'].includes(getCharAtName(name))) return 'info';
-  if (['K', 'D', 'Y', 'B', 'O'].includes(getCharAtName(name))) return 'success';
-  if (['P', 'E', 'R', 'S', 'U'].includes(getCharAtName(name))) return 'warning';
-  if (['V', 'W', 'X', 'M', 'Z'].includes(getCharAtName(name))) return 'error';
+  return name && `${name.charAt(0).toUpperCase()}${name.charAt(1).toUpperCase()}`;
+}
+
+const getColorByName = (name, other = {}) => {
+  if (['A', 'N', 'H', 'L', 'Q', '1', '6', '11', '16', '21', '26'].includes(getCharAtName(name, other.custom))) return 'primary';
+  if (['F', 'G', 'T', 'I', 'J', '2', '7', '12', '17', '22', '27'].includes(getCharAtName(name, other.custom))) return 'info';
+  if (['K', 'D', 'Y', 'B', 'O', '3', '8', '13', '18', '23', '28'].includes(getCharAtName(name, other.custom))) return 'success';
+  if (['P', 'E', 'R', 'S', 'U', '4', '9', '14', '19', '24', '29'].includes(getCharAtName(name, other.custom))) return 'warning';
+  if (['V', 'W', 'X', 'M', 'Z', '5', '10', '15', '20', '25', '30'].includes(getCharAtName(name, other.custom))) return 'error';
   return 'default';
 };
 
 const CustomAvatar = forwardRef(({ color, name = '', BadgeProps, children, sx, ...other }, ref) => {
   const theme = useTheme();
 
-  const charAtName = getCharAtName(name);
+  const charAtName = getCharAtName(name, other.custom);
 
-  const colorByName = getColorByName(name);
+  const colorByName = getColorByName(name, other);
 
   const colr = color || colorByName;
 
