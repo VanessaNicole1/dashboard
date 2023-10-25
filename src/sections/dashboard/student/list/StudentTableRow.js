8e46a3cfd5733f37c6498668a8939f72eec2be80
VanessaNicole1 <nicolegualan@gmail.com>
2023-10-24 23:33:50 -0500

    Remove edit option

--- src/sections/dashboard/student/list/StudentTableRow.js
+++ src/sections/dashboard/student/list/StudentTableRow.js
@@ -1,21 +1,15 @@
-import { useState } from 'react';
-import PropTypes from 'prop-types';
-import {
-  TableRow,
-  MenuItem,
-  TableCell,
-  IconButton,
-} from '@mui/material';
-import Iconify from '../../../../components/iconify';
-import MenuPopover from '../../../../components/menu-popover';
+import { useState } from "react";
+import PropTypes from "prop-types";
+import { TableRow, MenuItem, TableCell, IconButton } from "@mui/material";
+import Iconify from "../../../../components/iconify";
+import MenuPopover from "../../../../components/menu-popover";
 
 StudentTableRow.propTypes = {
   row: PropTypes.object,
   selected: PropTypes.bool,
-  onEditRow: PropTypes.func,
 };
 
-export default function StudentTableRow({ row, selected, onEditRow }) {
+export default function StudentTableRow({ row, selected }) {
   const { grade, user } = row;
   const [openPopover, setOpenPopover] = useState(null);
 
@@ -31,38 +25,14 @@ export default function StudentTableRow({ row, selected, onEditRow }) {
     <>
       <TableRow hover selected={selected}>
         <TableCell align="center">{user.name}</TableCell>
-        <TableCell align="center" sx={{ textTransform: 'capitalize' }}>
+        <TableCell align="center" sx={{ textTransform: "capitalize" }}>
           {user.lastName}
         </TableCell>
+        <TableCell align="center">{user.email}</TableCell>
         <TableCell align="center">
-          {user.email}
-        </TableCell>
-        <TableCell align="center">
-          { `${grade.number} "${grade.parallel}"` }
-        </TableCell>
-        <TableCell align="center">
-          <IconButton color={openPopover ? 'inherit' : 'default'} onClick={handleOpenPopover}>
-            <Iconify icon="eva:more-vertical-fill" />
-          </IconButton>
+          {`${grade.number} "${grade.parallel}"`}
         </TableCell>
       </TableRow>
-
-      <MenuPopover
-        open={openPopover}
-        onClose={handleClosePopover}
-        arrow="right-top"
-        sx={{ width: 140 }}
-      >
-        <MenuItem
-          onClick={() => {
-            onEditRow();
-            handleClosePopover();
-          }}
-        >
-          <Iconify icon="eva:edit-fill" />
-          Edit
-        </MenuItem>
-      </MenuPopover>
     </>
   );
-};
+}
