3d2e796f5b6c84a3af83cb7663e6b81c8f4e0c64
VanessaNicole1 <nicolegualan@gmail.com>
2023-10-29 20:37:14 -0500

    feat: Add Alert and Login Page

--- src/sections/auth/Login.js
+++ src/sections/auth/Login.js
@@ -1,31 +1,32 @@
-import { Tooltip, Stack, Typography, Box } from '@mui/material';
-import { useAuthContext } from '../../auth/useAuthContext';
-import LoginLayout from '../../layouts/login';
-import AuthLoginForm from './AuthLoginForm';
+import { Tooltip, Stack, Typography, Box } from "@mui/material";
+import { useAuthContext } from "../../auth/useAuthContext";
+import LoginLayout from "../../layouts/login";
+import AuthLoginForm from "./AuthLoginForm";
 
 export default function Login() {
   const { method } = useAuthContext();
 
   return (
-    <LoginLayout title='Plan de Clases'>
-      <Stack spacing={2} sx={{ mb: 5, position: 'relative' }}>
-        <Typography variant="h4">Ingresar al Sistema</Typography>
+    <LoginLayout title="Plan de Clases">
+      <Stack
+        sx={{
+          marginTop: "100px",
+        }}
+      >
+        <Stack spacing={2} sx={{ mb: 5, position: "relative" }}>
+          <Typography variant="h4">Ingresar al Sistema</Typography>
 
-        <Tooltip title={method} placement="left">
-          <Box
-            component="img"
-            alt={method}
-            src={`/assets/icons/auth/ic_${method}.png`}
-            sx={{ width: 32, height: 32, position: 'absolute', right: 0 }}
-          />
-        </Tooltip>
+          <Tooltip title={method} placement="left">
+            <Box
+              component="img"
+              alt={method}
+              src={`/assets/icons/auth/ic_${method}.png`}
+              sx={{ width: 32, height: 32, position: "absolute", right: 0 }}
+            />
+          </Tooltip>
+        </Stack>
+        <AuthLoginForm />
       </Stack>
-
-      {/* <Alert severity="info" sx={{ mb: 3 }}>
-        Use email : <strong>demo@minimals.cc</strong> / password :<strong> demo1234</strong>
-      </Alert> */}
-
-      <AuthLoginForm />
     </LoginLayout>
   );
 }
