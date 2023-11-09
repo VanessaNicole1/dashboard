507e89822f1bb79574ad023532489dbac0065d4a
VanessaNicole1 <nicolegualan@gmail.com>
2023-11-08 21:49:32 -0500

    convert string

--- src/keycloak.js
+++ src/keycloak.js
@@ -1,7 +1,7 @@
 import Keycloak from "keycloak-js";
 
 export const keycloak = new Keycloak({
-  url: process.env.KEYCLOAK_URL,
+  url: process.env.KEYCLOAK_URL + "",
   realm: "master",
   clientId: "lessonPlan",
 });
